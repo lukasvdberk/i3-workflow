d48a45d07cae40fcd630eddf616dc63a529f4b91
lukasberk <lukasvdberk@gmail.com>
2021-07-04 14:43:45 +0200

    Added application model

---
+++ models/application.py
@@ -0,0 +1,10 @@
+class Application:
+    def __init__(self, execution_path: str):
+        self.execution_path = execution_path
+
+
+    def set_execution_path(self, execution_path):
+        self.execution_path = execution_path
+
+    def get_execution_path(self):
+        return self.execution_path
