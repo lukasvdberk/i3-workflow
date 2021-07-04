ec39058f427dacc5ed68b2baad422f0c54e869f1
lukasberk <lukasvdberk@gmail.com>
2021-07-04 18:36:25 +0200

    Provided testing main

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from models.app_config import ApplicationConfig
+from models.application import Application
+from models.application_spawner import ApplicationSpawner
+from models.workspace import Workspace
+from modules.runner.app_runner import ApplicationRunner
+
+
 def main():
-    pass
+    app_spawners = [
+        ApplicationSpawner(Workspace("5:2"), Application("/usr/bin/firefox", ["Firefox", "firefox"]), 1)
+    ]
+    # not used yet
+    all_workspaces = []
+    app_config = ApplicationConfig(app_spawners, all_workspaces)
+
+    app = ApplicationRunner(app_config)
+    app.spawn_work_spaces()
 
 
 if __name__ == '__main__':
