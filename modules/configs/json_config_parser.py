aa762c0eb0807e78d6eb6b4b2f4a47f57c6cb95a
lukasberk <lukasvdberk@gmail.com>
2021-07-04 19:29:29 +0200

    From JSON file to locale objects

--- modules/configs/json_config_parser.py
+++ modules/configs/json_config_parser.py
@@ -1,8 +1,9 @@
-from __future__ import print_function
 import json
-from types import SimpleNamespace as Namespace
-
 from default_config import DEFAULT_JSON_FILE_LOCATION
+from models.app_config import ApplicationConfig
+from models.application import Application
+from models.application_spawner import ApplicationSpawner
+from models.workspace import Workspace
 from modules.configs.config_parser import IConfigParser
 
 
@@ -23,4 +24,21 @@ class JSONFileConfigParser(IConfigParser):
 
     def get_config(self):
         with open(DEFAULT_JSON_FILE_LOCATION, 'r') as json_config_file:
-            return json.load(json_config_file, object_hook=lambda d: Namespace(**d))
+            list_of_config_dic = json.load(json_config_file)
+
+            return list(map(JSONFileConfigParser.create_from_dict, list_of_config_dic))
+
+    @staticmethod
+    def create_from_dict(config_dic):
+        return ApplicationConfig(
+            name=config_dic["name"],
+            all_workspaces=list(map(lambda w: Workspace(w["name"]), config_dic["all_workspaces"])),
+            application_spawners=list(map(lambda s:
+              ApplicationSpawner(
+                  Workspace(s["workspace"]["name"]),
+                  Application(s["application"]["execution_path"], s["application"]["window_names"]),
+                  s["amount"]
+              ),
+              config_dic["application_spawners"])
+            ),
+        )
