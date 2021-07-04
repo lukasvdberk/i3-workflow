524532d63e376ff7988080d647d572d5a374995c
lukasberk <lukasvdberk@gmail.com>
2021-07-04 16:51:56 +0200

    Created config type factory

---
+++ modules/configs/config_factory.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from modules.configs.json_config_parser import JSONFileConfigParser
+
+
+class ConfigTypes(Enum):
+    JSON_PARSER = 1
+
+
+class ConfigFactory:
+    @staticmethod
+    def get_default_parser():
+        return JSONFileConfigParser()
+
+    @staticmethod
+    def get_certain_parser(config_type: ConfigTypes.member_type):
+        if ConfigTypes.JSON_PARSER == config_type:
+            return JSONFileConfigParser()
--- modules/configs/json_config_parser.py
+++ modules/configs/json_config_parser.py
@@ -1,8 +1,20 @@
+from default_config import DEFAULT_JSON_FILE_LOCATION
 from modules.configs.config_parser import IConfigParser
 
 
 class JSONFileConfigParser(IConfigParser):
+    def __init__(self):
+        self.file_location = DEFAULT_JSON_FILE_LOCATION
+
     def __init__(self, file_location):
+        self.file_location = file_location
+
+    def set_file_location(self, file_location):
+        """
+        Sets file location of JSON file to parse
+        :param file_location: JSON parser
+        :return: None
+        """
         self.file_location = file_location
 
     def get_config(self):
