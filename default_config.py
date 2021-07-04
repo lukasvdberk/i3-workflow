a9bafe768cf592fd9a59778a3862ebabf4138e6d
lukasberk <lukasvdberk@gmail.com>
2021-07-04 20:48:49 +0200

    Commands now executed in subprocess

--- default_config.py
+++ default_config.py
@@ -1,3 +1,13 @@
+import json
+import sys
+
 DEFAULT_JSON_FILE_LOCATION = "/home/lukas/Nextcloud/Programming/i3GroupStart/config/config.json"
 I3_CONFIG_FILE = "/home/lukas/.config/i3/config"
 I3_BACKUP_CONFIG_FILE_LOCATION = "/home/lukas/.config/i3/config.from-groups.backup"
+
+json_config_location = sys.argv[0]
+with open(json_config_location, 'r') as json_file:
+    json_config = json.load(json_file)
+    DEFAULT_JSON_FILE_LOCATION = json_config_location
+    I3_CONFIG_FILE = json_config["i3ConfigFile"]
+    I3_BACKUP_CONFIG_FILE_LOCATION = json_config["i3BackupFileLocation"]
--- modules/helpers/command_executor.py
+++ modules/helpers/command_executor.py
@@ -3,4 +3,4 @@ import subprocess
 
 
 def execute_os_command(command: str):
-    subprocess.check_call(shlex.split(command), stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    subprocess.Popen(shlex.split(command), stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
