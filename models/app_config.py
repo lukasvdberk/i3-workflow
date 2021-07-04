81d820bc390f8d417ad302db6382372bc0f3f933
lukasberk <lukasvdberk@gmail.com>
2021-07-04 16:40:36 +0200

    Added app config

---
+++ models/app_config.py
@@ -0,0 +1,14 @@
+from models.application_spawner import ApplicationSpawner
+from models.workspace import Workspace
+
+
+class AppConfig:
+    def __init__(self, application_spawners: list[ApplicationSpawner], all_workspaces: list[Workspace]):
+        self.application_spawners = application_spawners
+        self.all_workspaces = all_workspaces
+
+    def get_application_spawners(self):
+        return self.application_spawners
+
+    def get_all_workspaces(self):
+        return self.all_workspaces
