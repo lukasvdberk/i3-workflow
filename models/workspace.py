91a7564ee981f616e2f5129a54efec85dd7b130f
lukasberk <lukasvdberk@gmail.com>
2021-07-04 14:43:58 +0200

    Added setter with type definition

--- models/workspace.py
+++ models/workspace.py
@@ -1,5 +1,8 @@
 class Workspace:
-    def __init__(self, name):
+    def __init__(self, name: str):
+        self.name = name
+
+    def set_name(self, name: str):
         self.name = name
 
     def get_name(self):
